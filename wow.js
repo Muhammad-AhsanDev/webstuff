88b57838d603b94b47ddc6c91ea6fe18df98449a
Muhammad-AhsanDev <muhammadahsan2006k@gmail.com>
2025-11-26 11:20:02 +0500

    javascriptudated

--- wow.js
+++ wow.js
@@ -1,5 +1,16 @@
-console.log("NFILE")
-
-
-
-9
+console.log("STARTING")
+let wName=prompt("What is your name");
+let age=prompt("What is your age");
+let batch=prompt("What is your batch");
+console.log(wName)
+console.log(age)
+console.log(batch)
+console.log("NFILE")  
+let Name="Ahsan";
+let Age =12;
+let bOOL = true
+let x;
+let y=null
+let collection = [Name,Age,bOOL,x,y]
+let keyvalue={firstname:"Muhammad",lastname:"Ahsan",Email:"muhammadahsan2006",}
+console.log(keyvalue)
